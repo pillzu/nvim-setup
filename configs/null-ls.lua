e1a1ca013358aa8755bbf911b83a1a4d9c901193
Piyush Upadhyay <piyushupadhyay2566@gmail.com>
2023-05-18 11:20:50 -0400

    new setup

---
+++ configs/null-ls.lua
@@ -0,0 +1,54 @@
+local present, null_ls = pcall(require, "null-ls")
+
+if not present then
+  return
+end
+
+local b = null_ls.builtins
+
+local sources = {
+
+  -- webdev stuff
+  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+
+  -- Lua
+  b.formatting.stylua,
+
+  -- cpp
+  b.formatting.clang_format,
+
+  -- buf
+  b.diagnostics.buf,
+
+  -- python
+  b.formatting.autoflake,
+  b.formatting.autopep8,
+  b.formatting.buf,
+
+  -- golang
+  b.formatting.gofumpt,
+  b.formatting.goimports,
+  b.formatting.goimports_reviser,
+  b.formatting.golines,
+  b.diagnostics.golangci_lint,
+}
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+null_ls.setup {
+  debug = true,
+  sources = sources,
+  on_attach = function(client, bufnr)
+    -- on_atc(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format()
+        end,
+      })
+    end
+  end,
+}
---
+++ configs/overrides.lua
@@ -0,0 +1,78 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "go",
+    "python",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+  },
+  indent = {
+    enable = true,
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+
+    -- python
+    "buf",
+    "autoflake",
+    "autopep8",
+    "python-lsp-server",
+
+    -- go stuff
+    "gofumpt",
+    "goimports",
+    "goimports-reviser",
+    "golangci-lint",
+    "golangci-lint-langserver",
+    "golines",
+    "gopls",
+
+    "docker-compose-language-service",
+    "dockerfile-language-server",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+M.nvdash = {
+  load_on_startup = true,
+}
+
+return M
